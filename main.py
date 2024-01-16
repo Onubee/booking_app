b8712d8768cabe9ab5384b4ad50288b406ccd5ea
Komarov Sergey <onubee@gmail.com>
2024-01-16 16:11:03 +0300

    add models and bd

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Depends
+from typing import Optional
+from datetime import date
+from pydantic import BaseModel
+
+from bookings.router import router as booking_router
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(booking_router)
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+class HotelsSearchArgs:
+    def __init__(
+            self,
+            location: str,
+            date_from: date,
+            date_to: date,
+            has_spa: Optional[bool] = None,
+            stars: Optional[int] = Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
+@app.get("/hotels")
+def get_hotels(
+        search_args: HotelsSearchArgs = Depends()
+):
+    return search_args
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/bookings")
+def add_booking(booking: SBooking):
+    pass
